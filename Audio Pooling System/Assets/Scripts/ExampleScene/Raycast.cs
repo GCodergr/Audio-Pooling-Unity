b202edc4d24c3446d04bfa7dde3e9f486e494a04
Dimitris <dimitris_gd@hotmail.com>
2016-12-29 22:12:54 +0200

    Added Raycast and Button scripts

---
+++ Audio Pooling System/Assets/Scripts/ExampleScene/Raycast.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour
+{
+    private float raycastRange = 20f;
+    private Vector3 rayOrigin;
+
+    private Text notificationText;
+    private Camera fpsCamera;
+
+    void Awake()
+    {
+        notificationText = GameObject.Find("Canvas/Notification Text").GetComponent<Text>();
+        fpsCamera = GetComponent<Camera>();
+
+
+    }
+
+    void Start()
+    {
+        StartCoroutine(HideTutorialMessage());
+    }
+
+    void Update()
+    {
+        CheckForButton();
+    }
+
+    private void CheckForButton()
+    {
+        rayOrigin = fpsCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, fpsCamera.transform.forward, out hit, raycastRange))
+        {
+            //if (hit.collider.CompareTag)
+            //{
+
+            //}
+        }
+    }
+
+    private IEnumerator HideTutorialMessage()
+    {
+        yield return new WaitForSeconds(2f);
+
+        notificationText.text = "";
+    }
+    
+
+}
